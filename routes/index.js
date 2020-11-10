a662328686144629de81091601b2727445993e7f
whgusqls007 <jo7hb@naver.com>
2020-11-10 21:34:58 +0900

    configuration 수정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Money Gang Project' });
+  res.render('index', { title: 'Money Gang Project.' });
 });
 
 module.exports = router;
