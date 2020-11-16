0c2b6df489608902b030213b7ae689a8bedcd748
jsy0404 <37055726+jsy0404@users.noreply.github.com>
2020-11-17 01:27:07 +0900

    Add .gitignore (#7)
    * Add Codacy badge (#2)
    
    * Testing for HTML, New page, Onclick (#4)
    
    * Did Until Hello Money Gang
    
    * configuration 수정
    
    * configuration 수정
    
    * type : test
    
    test for html style, new page and other function
    
    * chore : Edit .gitignore
    
    * style : fit with codacy
    
    Co-authored-by: Codacy Badger <badger@codacy.com>
    Co-authored-by: whgusqls007 <54840668+whgusqls007@users.noreply.github.com>
    Co-authored-by: jsy0404 <pnmsegwyd@naver.com>

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -21,12 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -37,5 +38,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
