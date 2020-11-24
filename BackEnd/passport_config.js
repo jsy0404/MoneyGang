7f6755f9a74d185fae3c678000dc9f04594b15bb
whgusqls007 <54840668+whgusqls007@users.noreply.github.com>
2020-11-24 18:15:21 +0900

    Create BackEnd (#11)
    * Google Login (#1)
    
    * Google Login (#1)
    
    * Google Login (#1)
    
    * Delete google.json
    
    * Google Login (#3)
    
    * Google Login (#3)
    
    * Here's your README
    
    * Mongo Database (#1)
    
    * BackEnd
    
    * BackEnd
    
    * Move to BackEnd Folder
    
    * Move to BackEnd Folder (#2)
    
    * Move to BackEnd Folder (#3)
    
    * Move to BackEnd Folder (#3)

--- BackEnd/passport_config.js
+++ BackEnd/passport_config.js
@@ -1,19 +1,19 @@
-const passport       = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const googleData     = require("./google.json");
-const ID             = googleData.web.client_id;
-const Secret         = googleData.web.client_secret;
-const callback       = googleData.web.redirect_uris[0];
+const passportconfig = require("passport");
+const GoogleStrategy  = require("passport-google-oauth20").Strategy;
+const googleData      = require("./google");
+const ID              = googleData.web.client_id;
+const Secret          = googleData.web.client_secret;
+const callback        = googleData.web.redirect_uris[0];
 
 
-passport.serializeUser(function (user, done){
+passportconfig.serializeUser(function (user, done){
     done(null, user);
 });
-passport.deserializeUser(function (user,done){
+passportconfig.deserializeUser(function (user, done){
     done(null, user);
 });
 
-passport.use(new GoogleStrategy({
+passportconfig.use(new GoogleStrategy({
         clientID     : ID,
         clientSecret : Secret,
         callbackURL  : callback
