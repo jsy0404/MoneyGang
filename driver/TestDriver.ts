af1580e41c4269daab0622cecba4dab35870882a
jsy0404 <pnmsegwyd@gmail.com>
2021-02-19 22:58:09 +0900

    feat : Add TestDriver.
    TestDriver is for simulate trading with deposit.
    
    - It has no slippage.
    - Just use only `Buy` or `Sell`
    - Default fee is 0.05% in Bidirectional.
      - Fee is taken with `Buy` side price.
      - Fee is taken at `Sell` time.

---
+++ driver/TestDriver.ts
@@ -0,0 +1,35 @@
+export
+class TestDriver {
+	Balance:	number;
+	lastPrice:	number;
+	division:	number;
+	fee:		number;
+
+	constructor() {
+		this.Balance = 100000;
+		this.division = 10;
+		this.fee = 0.0005;
+		this.lastPrice = 0;
+	}
+
+	getDivision(): number {
+		return this.Balance/this.division;
+	}
+
+	setDivision(division: number): void {
+		this.division = division;
+	}
+
+	Buy(price: number): void {
+		this.lastPrice = price;
+	}
+
+	Sell(price: number): void {
+		if (this.lastPrice == 0) {
+			return;
+		}
+		this.Balance += (((price - this.lastPrice) / this.lastPrice) - this.fee) * this.getDivision();
+		console.log("Balance: " + this.Balance);
+	}
+
+}
