bac9cb83dedeedbbc0648fc838eff1638684e833
jsy0404 <pnmsegwyd@gmail.com>
2021-03-04 23:29:26 +0900

    fix : Remove *.js

--- algorithm/AlgorithmModule.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Algorithm = void 0;
-class Algorithm {
-    constructor(receiver) { this.receiver = receiver; this.lastPrice = 0; }
-    setLastPrice(price) { this.lastPrice = price; }
-    ;
-}
-exports.Algorithm = Algorithm;
